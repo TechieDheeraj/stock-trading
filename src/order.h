4aab33d64c45224c7ef2c33e90950a235cf2e08c
Atmanand Nagpure <atmanandcollab@gmail.com>
2021-04-26 20:16:51 +0530

    Fix conflicts

--- src/order.h
+++ src/order.h
@@ -4,55 +4,60 @@
 #include "rapidjson/istreamwrapper.h"
 #include "rapidjson/document.h"
 
-class Order {
-  public:
-    std::string orderId;
-    std::string symbol; // AAPL, FB, AMZN ..etc
-    std::string assetId;
-    std::string exchange;
-    std::string assetType;
-    std::string accountId;
-    std::string portfolioId;
-    std::string side; // order: buy/sell
-    std::string orderType;
-    double quantity;
-    double price;
-    double auxPrice;
-    std::string timeInForce;
-    double stopPrice;
-    std::string routing;
-    std::string parentOrderId;
-    std::string bulkOrderId;
-    int status;
-    std::string altOrderId;
-    std::string rebalanceId;
-    std::string modelId;
-    std::string orderTime;
-    std::string updateDate;
+class Order
+{
+private:
+  std::string orderId;
+  std::string symbol; // AAPL, FB, AMZN ..etc
+  std::string assetId;
+  std::string exchange;
+  std::string assetType;
+  std::string accountId;
+  std::string portfolioId;
+  std::string side; // order: buy/sell
+  std::string orderType;
+  double quantity;
+  double price;
+  double auxPrice;
+  std::string timeInForce;
+  double stopPrice;
+  std::string routing;
+  std::string parentOrderId;
+  std::string bulkOrderId;
+  int status;
+  std::string altOrderId;
+  std::string rebalanceId;
+  std::string modelId;
+  std::string orderTime;
+  std::string updateDate;
 
-  public:
-    friend class OrderBook;
-    friend class MatchingEngine;
-    std::string serialise();
-    void deSerialise(rapidjson::Value& val);
-    inline bool is_buy() { return (this->side == "buy") ? true: false; }
-    bool order_qty();
-    bool getPrice();
-    long epochtime;
+public:
+  friend class OrderBook;
+  friend class MatchingEngine;
+  std::string serialise();
+  void deSerialise(rapidjson::Value &val);
+  inline bool is_buy() { return (this->side == "buy") ? true : false; }
+  bool order_qty();
+  bool getPrice();
+  long epochtime;
 
-    Order() { epochtime = 0; }
+  Order() { epochtime = 0; }
 };
 
-template<typename T> inline T sanitize(rapidjson::Value& v) {}
-template<>
-inline std::string sanitize<std::string>(rapidjson::Value& v) {
+template <typename T>
+inline T sanitize(rapidjson::Value &v) {}
+template <>
+inline std::string sanitize<std::string>(rapidjson::Value &v)
+{
   return v.IsNull() ? "" : v.GetString();
 }
-template<>
-inline double sanitize<double>(rapidjson::Value& v) {
+template <>
+inline double sanitize<double>(rapidjson::Value &v)
+{
   return v.IsNull() ? 0.0 : v.GetDouble();
 }
-template<>
-inline int sanitize<int>(rapidjson::Value& v) {
+template <>
+inline int sanitize<int>(rapidjson::Value &v)
+{
   return v.IsNull() ? 0 : v.GetInt();
 }
